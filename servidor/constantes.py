7090a86c79bb55567de0c3964cf8bae75cd82414
Avique Caxinguelê - Duritia <duritia@yahoo.com>
2022-07-05 16:47:36 -0300

    Cria o arquivo "servidor/constantes.py" contendo constantes para o projeto.

---
+++ servidor/constantes.py
@@ -0,0 +1,24 @@
+# Arquivo com constantes para o projeto
+
+# Para bicicletas:
+BicicletaPronta = 0
+BicicletaAlugada = 1
+BicicletaEstragada = 2
+BicicletaEmConserto = 3
+BicicletaNãoDevolvida = 4       # Não devolvida *no prazo*
+PreçoPorHora = 2.50             # Preço a pagar por hora de aluguel
+PreçoPorHoraDeAtraso = 4.50     # Preço a pagar por hora de atraso
+
+# Para estações
+EstaçãoNãoDefinida = 0
+EstaçãoVendaNova = 1
+EstaçãoPalmares = 2
+EstaçãoPrado = 3
+EstaçãoSãoGabriel = 4
+EstaçãoCentral = 5
+EstaçãoSion = 6
+DataDesconsiderada = 1721059.5  # Equivale à data  0000/01/01 00:00:00. Veja:
+                                # https://sqlite.org/lang_datefunc.html
+ 
+
+# vim: fileencoding=utf-8: expandtab: shiftwidth=4: tabstop=8: softtabstop=4
